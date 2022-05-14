a5410ce6b53105a403fb79068974e73695780c5d
Will <twitchifygamers@gmail.com>
2022-05-14 18:52:51 +0100

    Created view stuff for chat

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,35 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+
+Vue.component('chat-messages', require('../views/ChatMessages'));
+Vue.component('chat-form', require('../views/ChatForm'));
+
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        messages: []
+    },
+
+    created() {
+        this.fetchMessages();
+    },
+
+    methods: {
+        fetchMessages() {
+            axios.get('/messages').then(response => {
+                this.messages = response.data;
+            });
+        },
+
+        addMessage(message) {
+            this.messages.push(message);
+
+            axios.post('/messages', message).then(response => {
+                console.log(response.data);
+            });
+        }
+    }
+});
